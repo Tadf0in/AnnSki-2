9860d4f95ae68d64b96d5bc1994de826b8486140
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-10-03 22:43:31 +0200

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Home from './components/Home/Home'
+import Events from './components/Events/Events'
+import Shop from './components/Shop/Shop'
+import Profile from './components/Profile/Profile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
+      <Navbar />
       <Routes>
-        <Route path='/' element='home' />      
+        <Route path='/' element={<Home />} /> 
+        <Route path='/events' element={<Events />} />      
+        <Route path='/shop' element={<Shop />} />  
+        <Route path='/profile' element={<Profile />} />      
       </Routes>
     </div>
   )
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className='navbar navbar-expand-md bg-body-tertiary'>
+        <div className="container-fluid">
+            <Link to="/" className="navbar-brand">
+                <img src={logo} className='d-inline-block' alt="Logo" width="50" height="50"/>
+                &nbsp; Ann'ski
+            </Link>
+
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mb-2">
+                    <li className="nav-item">
+                    <Link to="/" className='nav-link'>Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/events" className='nav-link'>Sorties</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/shop" className='nav-link'>Boutique</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
   )
 }
