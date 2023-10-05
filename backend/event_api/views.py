5cbb0cddadd372c7b747899248cadb8f7ee854bd
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-10-05 18:22:26 +0200

    GET events

--- backend/event_api/views.py
+++ backend/event_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.views import Response, status, APIView
+from .serializers import *
+from .models import *
 
-# Create your views here.
+
+class EventsView(APIView):
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
+
+    def get(self, request):
+        events = Event.objects.all()
+        serializer = EventSerialier(events, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
