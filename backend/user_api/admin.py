c58068ada1c72611c77927e9b2faf9a079d88e95
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-10-08 19:56:00 +0200

    Inscription Model

--- backend/user_api/admin.py
+++ backend/user_api/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+# User Inline
+class MembreInline(admin.StackedInline):
+    model = Membre
+    can_delete = False
+
+class UserMembreAdmin(UserAdmin):
+    inlines = [MembreInline]
+
+    list_display = ['name', 'adherent']
+    search_fields = ['first_name', 'last_name', 'email', 'membre__tel']
+    list_filter = ('membre__is_adherent',)
+
+    def name(self, user):
+        return f"{user.first_name} {user.last_name} ({user.username})"
+    
+    def adherent(self, user):
+        return user.membre.is_adherent
+    adherent.boolean = True
+
+admin.site.unregister(User)
+admin.site.register(User, UserMembreAdmin)
+
+
+@admin.register(Membre)
+class MembreAdmin(admin.ModelAdmin):
+    search_fields = ['user__first_name', 'user__last_name', 'user__email', 'tel']
--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+user_model = get_user_model()
+
+
+class Membre(models.Model):
+    user = models.OneToOneField(user_model, on_delete=models.CASCADE)
+    
+    tel = models.CharField(max_length=12, verbose_name="Numéro de téléphone")
+    is_adherent = models.BooleanField(verbose_name="Adhérent", default=False)
+    num_usca = models.CharField(max_length=20, null=True, blank=True, verbose_name="Numéro carte USCA")
+
+    
+    def __str__(self) -> str:
+        return f"{self.user.first_name} {self.user.last_name} ({self.user.username})"
+
