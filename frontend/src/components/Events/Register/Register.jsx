0eff65104bf2687ef151eca273ccc25b052e18cf
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-12-24 19:46:50 +0100

    new form

--- frontend/src/components/Events/Register/Register.jsx
+++ frontend/src/components/Events/Register/Register.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useParams } from "react-router-dom"
 import useFetch from '../../../hooks/useFetch'
 import Loading from '../../../utils/Loading'
-import NAForm from './NAForm'
 import Header from './Header'
-import AForm from './AForm'
+import Form from './Form'
+
 
 export default function Register () {
     const {event_id} = useParams()
     const {loading, data, errors} = useFetch('/api/events/'+event_id, {method: 'GET'})
-    const [adherent, setAdherent] = useState(null)
 
     if (errors) {
         throw new Error("Cette sortie ne semble pas exister")
@@ -24,15 +23,7 @@ export default function Register () {
         { data && data.can_register && 
         <div className='register d-flex flex-column align-items-center'>
             <Header data={data} />
-
-            { adherent === null ? 
-                <div className='btns-adherent d-grid gap-3 mx-auto mt-5'>
-                    <button className='btn btn-adherent btn-primary' onClick={() => setAdherent(true)}>Adhérent ({data.prixA}€)</button>
-                    <button className='btn btn-adherent btn-secondary' onClick={() => setAdherent(false)}>Non Adhérent ({data.prixNA}€)</button>
-                </div>
-
-                : adherent ? <AForm /> : <NAForm />
-            }
+            <Form />
         </div>
         }
     </>
