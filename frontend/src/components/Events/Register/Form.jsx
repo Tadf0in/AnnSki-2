081341fe083891329f20ae9651246c2753546eca
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-12-25 12:36:43 +0100

    Formdata register front OK

--- frontend/src/components/Events/Register/Form.jsx
+++ frontend/src/components/Events/Register/Form.jsx
@@ -12,18 +12,25 @@ export default function Form() {
     ecole: "",
   })
 
-  return <form className='register-form'>
-      <FormSelect name="adherent" placeholder="Adhérent ?" get={formData} set={setFormData} options={[
+  const submitForm = (e) => {
+    e.preventDefault()
+    console.log(formData)
+  }
+
+  return <form className='register-form' onSubmit={e => submitForm(e)}>
+      <FormSelect key="form-select-register-adherent" name="adherent" placeholder="Adhérent ?" 
+      get={formData} set={setFormData} options={[
         ["false", "Non adhérent"], 
         ["true", "Adhérent"]
       ]}/>
 
-      <FormFloating type="text" id="lastName" placeholder="Nom" />
-      <FormFloating type="text" id="firstName" placeholder="Prénom" />
-      <FormFloating type="mail" id="mail" placeholder="Adresse mail" />
-      <FormFloating type="tel" id="tel" placeholder="Numéro de téléphone" />
+      <FormFloating type="text" name="nom" id="formFLoating-register-nom" placeholder="Nom" get={formData} set={setFormData}/>
+      <FormFloating type="text" name="prenom" id="formFLoating-register-prenom" placeholder="Prénom" get={formData} set={setFormData}/>
+      <FormFloating type="mail" name="mail" id="formFLoating-register-mail" placeholder="Adresse mail" get={formData} set={setFormData}/>
+      <FormFloating type="tel" name="tel" id="formFLoating-register-tel" placeholder="Numéro de téléphone" get={formData} set={setFormData}/>
 
-      <FormSelect name="ecole" placeholder="École" get={formData} set={setFormData} options={[
+      <FormSelect key="form-select-register-ecole" name="ecole" placeholder="École" 
+      get={formData} set={setFormData} options={[
         ["Polytech", "Polytech"],
         ["IUT", "IUT"],
         ["IAE", "IAE"],
--- frontend/src/utils/Fields.jsx
+++ frontend/src/utils/Fields.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 
-export default function FormFloating({ type, id, placeholder }) {
+
+export default function FormFloating({ type, name, get, set, id, placeholder }) {
   return (
     <div className="form-floating mb-3">
-        <input type={type} className="form-control" id={"floating"+id} placeholder={placeholder} required/>
+        <input type={type} className="form-control" id={"floating"+id} placeholder={placeholder} required
+        value={get[[name]]}
+        onChange={e => set({...get, [name]: e.target.value})}/>
         <label htmlFor={"floating"+id}>{placeholder}</label>
     </div>
   )
 }
 
+
 export function FormSelect({ name, placeholder, get, set, options }) {
-  return <select className='form-select mb-3' name={name} value={get[[name]]} defaultValue="" required
-  onChange={e => set({get, [name]: e.target.value})}>
-    <option disabled value="">{placeholder}</option>
-    { options.map((o, i) => {
-      return <>
+  return <select className='form-select mb-3' name={name} defaultValue="" required
+  value={get[[name]]}
+  onChange={e => set({...get, [name]: e.target.value})}>
+    <option key={"select-"+name+"-default"} disabled value="">{placeholder}</option>
+    { options.map((o, i) => 
       <option key={"select-"+name+"-"+i} value={o[0]}>{o[1]}</option>
-      </>
-    })}
+    )}
   </select>
 }
