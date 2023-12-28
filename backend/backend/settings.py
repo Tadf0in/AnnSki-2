6506d2a587d06d25201da08788b36157a592cba3
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-12-28 14:07:15 +0100

    config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$6z=98s&4(9kx3k(phyd%wr6!m1!@06^%zd@px#!+^6clt2pah'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,14 +88,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': config('ENGINE'),
-        'NAME': config('NAME'), 
-        'USER': config('USER'), 
-        'PASSWORD': config('PASSWORD'), 
-        'HOST': config('HOST'), 
-        'PORT': config('PORT'), 
+        dj_database_url.config(default=config('DATABASE_URL')),
+
+        # 'ENGINE': config('ENGINE'),
+        # 'NAME': config('NAME'), 
+        # 'USER': config('USER'), 
+        # 'PASSWORD': config('PASSWORD'), 
+        # 'HOST': config('HOST'), 
+        # 'PORT': config('PORT'), 
     }
 }
 
