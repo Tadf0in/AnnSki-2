bfdfefdc12b6934d8bbc6a81d05112311035fb1d
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2023-12-31 15:06:23 +0100

    rm frontend

--- backend/settings.py
+++ backend/settings.py
@@ -141,9 +141,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'docs/assets')
-]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
