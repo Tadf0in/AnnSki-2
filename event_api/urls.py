f6512ab5f7c27bd144f6fb569d30759b9ceac1fb
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2024-01-03 14:53:52 +0100

    evnet/id rm

--- event_api/urls.py
+++ event_api/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.EventsView.as_view(), name='events'),
-    path('<int:event_id>', views.EventView.as_view(), name='next-event'),
     path('register/<int:event_id>', views.RegisterView.as_view(), name='register-event'),
 ]
