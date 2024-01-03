fe4f53a30b68fa83676202f5bba9a75b6eac2ad7
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2024-01-03 23:28:28 +0100

    download_csv admin

--- event_api/admin.py
+++ event_api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.http.response import HttpResponse
 from .models import *
+import csv
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
@@ -16,3 +18,22 @@ class InscriptionAdmin(admin.ModelAdmin):
     
     list_editable = ('paye', 'present_aller', 'present_retour')
     autocomplete_fields = ('membre', 'sortie')
+
+    actions = ('download_csv',)
+
+    def download_csv(self, request, queryset):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = f'attachment; filename=Inscriptions {queryset[0].sortie.lieu}.csv'
+        
+        field_names = ["Aller", "Retour", "Nom", "Prenom", "Adherent", "Commentaires", "Mail", "Tel", "Payé"]
+        writer = csv.writer(response)
+        writer.writerow(field_names)
+        
+        for obj in queryset:
+            aller = False if obj.present_aller else "/"
+            retour = False if obj.present_retour else "/"
+            writer.writerow([aller, retour, obj.membre.nom, obj.membre.prenom, bool(obj.membre.adherent), "", obj.membre.mail, obj.membre.tel, obj.paye])
+    
+        return response
+    
+    download_csv.short_description = "Télécharger le tableau"
