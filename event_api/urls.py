08431fd9dfc6e8afbf672c7b395d1b2d47a0df95
Louis AMOUDRUZ <louis.amoudruz@gmail.com>
2024-01-03 16:13:33 +0100

    api get event

--- event_api/urls.py
+++ event_api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.EventsView.as_view(), name='events'),
+    path('<int:event_id>', views.EventView.as_view(), name='event'),
     path('register/<int:event_id>', views.RegisterView.as_view(), name='register-event'),
 ]
